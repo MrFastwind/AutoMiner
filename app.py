120d5a04355804f900a7448e962e748aa9f06c60
MrFastwind <fastwind.k@gmail.com>
2021-06-03 00:05:16 +0200

    Adds Stop function, fixes properties with lambdas call, adds thread for continius cheking

--- app.py
+++ app.py
@@ -3,17 +3,32 @@ import signal
 import sys
 import psutil
 import subprocess
+import threading
+import time
+from pathlib import Path
 import pandas as pd
 from typing import Union
+
+FILEPATH = Path("D:\Mining\lolMiner\lolMiner.exe")
+ARGS = list(("--config", Path("D:\Mining\lolMiner\lolMiner.cfg"),
+            "--algo", "ETCHASH",
+             "--pool", "eu1-etc.ethermine.org:4444"))
 
 
 class App:
 
-    __process_manager: object
+    __alive: bool
+    __sleep_time: int
+    alive = property(lambda self: self.__alive)
 
     def __init__(self):
         signal.signal(signal.SIGINT, self.signalHandler)
-        self.__process_manager(FileLoader.loadFile("ProgramList.txt"))
+        self.__process_searcher = ProcessSearcher(
+            FileLoader.loadFile("ProgramList.txt"))
+        self.__miner = Miner(file=FILEPATH, args=ARGS)
+        self.__thread_checker = threading.Thread(target=self.checker)
+        self.__alive = False
+        self.__sleep_time = 30
 
     def signalHandler(self, sig, frame):
         self.stop()
@@ -21,17 +36,24 @@ class App:
         sys.exit(0)
 
     def start(self):
+        self.__alive = True
+        self.__thread_checker.start()
+        # signal.pause()
 
-        # init miner
-
-        # check processlist
-
-        # start daemon checker
-
-        signal.pause()
+    def checker(self):
+        while self.__alive:
+            if(self.__miner.alive and self.__process_searcher.searchInList()):
+                self.__miner.stop()
+                print("Miner Stopped!")
+            elif(not self.__miner.alive and not self.__process_searcher.searchInList()):
+                print("Miner Started!")
+                self.__miner.start()
+            time.sleep(self.__sleep_time)
+        self.__miner.stop()
 
     def stop(self):
-        ...
+        self.__alive = False
+        self.__miner.stop()
 
 
 class FileLoader:
@@ -41,7 +63,7 @@ class FileLoader:
 
 class ProcessSearcher:
     _process_list: set[str]
-    process_list = property(_process_list)
+    process_list = property(lambda self: self._process_list)
 
     def __init__(self, process_list: Union[list, set, str] = None):
         self._process_list = set(process_list)
@@ -58,9 +80,9 @@ class Miner:
     __args: list[str]
     __process: subprocess.Popen
     __alive: bool = False
-    filepath = property(__filepath)
-    args = property(__args)
-    alive = property(__alive)
+    filepath = property(lambda self: self.__filepath)
+    args = property(lambda self: self.__args)
+    alive = property(lambda self: self.__alive)
 
     def __init__(self, file: str, args: list):
         self.__filepath, self.__args = file, args
